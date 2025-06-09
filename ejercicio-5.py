bf49f84fb3deb8c416c29610568cc32f854992d1
FjC-14 <fcastrol@unah.hn>
2025-06-08 23:05:20 -0600

    Update ejercicio-5.py

--- ejercicio-5.py
+++ ejercicio-5.py
@@ -3,16 +3,14 @@ ventas = {
     'Cantidad': [10, 5, 7, 3, 2, 8]
 }
 
-
 ventas_totales = {}
-
 
 for i in range(len(ventas['Producto'])):
     producto = ventas['Producto'][i]
     cantidad = ventas['Cantidad'][i]
 
     if producto in ventas_totales:
-        ventas_totales[producto] += cantidad
+        ventas_totales[producto] = ventas_totales[producto] + cantidad
     else:
         ventas_totales[producto] = cantidad
 
